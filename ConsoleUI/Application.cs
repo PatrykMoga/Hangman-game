fc4f98eb53a9ba811862ff3d42cad9bf24947bda
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-03-06 08:28:01 +0100

    ct

---
+++ ConsoleUI/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Application 
+    {
+      
+        public Application()
+        {
+            
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
